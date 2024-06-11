779b2dbf9b7d14cfcf172d7e3be4e7585404b900
Valijon <parliament888@gmail.com>
2024-06-12 00:16:53 +0500

    prod

--- _nuxt/CuOrzbQg.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as e,o as t}from"./CzKKRTsN.js";const n={};function o(r,s){return t(),e("h1",null,"Контакты")}const _=c(n,[["render",o]]);export{_ as default};
--- _nuxt/FavDPlcU.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./CzKKRTsN.js";const t={};function n(o,a){return r(),c("h1",null,"Тарифы")}const _=e(t,[["render",n]]);export{_ as default};
